504d4cdd0314df9c6c1c69174898fa42eb768c1e
valentimarco <gamespy67@gmail.com>
2023-07-07 19:46:29 +0200

    Modified script sh

--- Progetto Compilatori/src/main.sh
+++ Progetto Compilatori/src/main.sh
@@ -3,11 +3,16 @@ echo "Compiling flex script"
 cd ./flex
 flex LessicalParser.fl
 
-echo "Compiling lex.yy file"
-gcc LessicalParser.c -o ../main.out
-rm LessicalParser.c
 cd ../
 
-echo "Scanning of input1"
-./main.out ./input/input1.txt ./output/input1.log
-rm main.out
+echo "Compiling bison script..."
+cd ./bison
+bison SintaticsParser.y -d
+
+cd ../
+echo "Compiling all files..."
+gcc ./flex/LessicalParser.c ./bison/SintaticsParser.tab.c ./bison/SintaticsParser.tab.h -o main.out
+
+
+./main.out ./input/input1.txt
+rm main.out ./flex/LessicalParser.c ./bison/SintaticsParser.tab.c ./bison/SintaticsParser.tab.h
